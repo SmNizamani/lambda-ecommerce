966cdad61063b4fd682a01035c7cfee5a8e7e971
SmNizamani <62019584+SmNizamani@users.noreply.github.com>
2024-08-26 03:21:31 +0200

    Refactored Login cases logic

--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -1,42 +1,27 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { chromium, Page, Browser, expect } from "@playwright/test"
+import { LoginPage } from "../pages/LoginPage";
+import { pageFixture } from "../hooks/pageFixture";
 
 
-let browser: Browser;
-let page: Page;
+let loginPage = new LoginPage(pageFixture.page)
 Given('User navigates to website', async function () {
-    browser = await chromium.launch({ headless: false });
-    page = await browser.newPage();
-    await page.goto("https://ecommerce-playground.lambdatest.io/")
+    await loginPage.goToWebpage();
+});
+
+Given('The user clicks on login account.', async function () {
+    await loginPage.clickOnLoginBtn();
+});
+
+When('user enter username as {string} and password as {string}', async function (username: string, password: string) {
+    await loginPage.fillLoginInfo(username, password);
 
 });
 
-Given('User click on account button', async function () {
-    await page.locator("//a[@role='button']//span[@class='title'][normalize-space()='My account']").click();
-    //    await page.locator("//span[normalize-space(text())='Login']").click();
+Then('{string} section should be displayed - SUCCESS', async function (featureSectionName: string) {
+    await expect(pageFixture.page.locator("div[id='content'] div:nth-child(1) h2:nth-child(1)")).toHaveText(featureSectionName)
 });
 
-
-Given('user enter username as {string}', async function (username) {
-    await page.locator('#input-email').fill(username);
-
-});
-
-Given('user enter password as {string}', async function (password) {
-    await page.locator('#input-password').fill(password);
-});
-
-When('user clicks on submit button', async function () {
-    await page.locator('input[value="Login"]').click()
-});
-
-Then('login should be successfull', async function () {
-    const currentUrl = page.url();
-    expect(currentUrl).toBe('https://ecommerce-playground.lambdatest.io/index.php?route=account/account');
-});
-
-Then('login should be failing', async function () {
-    //const errorMessage = await page.locator('.alert.alert-danger.alert-dismissible').textContent();
-    //expect(errorMessage).toContain('Warning: No match for E-Mail Address and/or Password.');
-    expect(page.locator('.alert.alert-danger.alert-dismissible')).toBeVisible();
+Then('Error message should be visible - FAILURE', async function () {
+    await expect(pageFixture.page.locator('.alert.alert-danger.alert-dismissible')).toBeVisible();
 });
